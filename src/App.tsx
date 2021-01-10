3899fe6f29539218d28c454bc630ee9f485869e6
Vaggelis Karavasileiadis <e.karavasileiadis@gmail.com>
2021-01-10 14:11:08 +0200

    Switching to styled components

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { registerRootComponent } from 'expo';
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  flex: 1;
+  background-color: '#fff';
+  align-items: center;
+  justify-content: center;
+`;
 
 const App = () => {
   return (
-    <View style={styles.container}>
+    <Container>
       <Text>Open to start working on your a!</Text>
       <StatusBar style="auto" />
-    </View>
+    </Container>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default registerRootComponent(App);
